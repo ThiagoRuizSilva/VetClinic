7a5df08977147127f6912baf7ca5952a1bf1d5f7
Thiago R <thiagoruiz397@gmail.com>
2024-05-07 12:09:36 -0300

    feat: create server and connection db

---
+++ server.js
@@ -0,0 +1,46 @@
+import express from 'express'
+import db  from './db/connection.js'
+import bodyParser from 'body-parser'
+import conection from './db/connection.js'
+const app = express()
+const PORT = 3333
+
+import tutorsRouter from './routes/tutors.js'
+import petsRouter from './routes/pets.js'
+//import { Pets } from './models/Pets.js'
+
+app.listen(PORT, function() {
+    console.log(`Server is running on port ${PORT}`)
+})
+
+// body
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+db.authenticate()
+.then(() => {
+    console.log("Conectou ao banco com sucesso")
+})
+.catch(err => {
+    console.log("Ocorreu um erro ao conectar", err)
+})
+
+
+conection
+.sync()
+//.sync({ force: true})
+.then(() => {
+    console.log("Sicronizado")
+}).catch(err => {
+    console.log(err);
+})
+
+
+
+app.get('/', (req, res) => {
+    res.send('Esta funcionando')
+})
+
+// all tutor routes
+app.use('/pets', petsRouter); 
+app.use('/tutors', tutorsRouter);
