f9a58c1d4731013cfcb5ee29592a7449f377635e
Thiago R <thiagoruiz397@gmail.com>
2024-05-07 12:09:55 -0300

    feat: create model pets

---
+++ models/Pets.js
@@ -0,0 +1,28 @@
+import Sequelize from 'sequelize'
+import db from '../db/connection.js'
+import Tutors from './Tutors.js'
+
+const Pets = db.define('Pets', {
+    name: {
+        type: Sequelize.STRING
+    },
+    species: {
+        type: Sequelize.STRING
+    },
+    carry: {
+        type: Sequelize.STRING
+    },
+    weight: {
+        type: Sequelize.INTEGER
+    },
+    date_of_birth: {
+        type: Sequelize.DATE
+    }
+
+
+})
+
+Tutors.hasMany(Pets)
+Pets.belongsTo(Tutors)
+
+export default Pets
