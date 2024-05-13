c1b5ef0c6d4cb287797352058a3f5a12e2934912
Thiago R <thiagoruiz397@gmail.com>
2024-05-13 17:01:32 -0300

    fix: swagger

--- src/swagger.js
+++ src/swagger.js
@@ -1,6 +1,6 @@
 import swaggerAutogen from "swagger-autogen";
 const outputFile = "./swagger_output.json";
-const endpointsFiles = ["./routes/pets.js", "./routes/tutors.js"];
+const endpointsFiles = ["./routes/petsRouter.js", "./routes/tutorsRouter.js"];
 
 swaggerAutogen(outputFile, endpointsFiles).then(() => {
   import("./server.js");
